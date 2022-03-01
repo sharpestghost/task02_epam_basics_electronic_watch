172d47507bc8f8c7b6c679fe6dcdcc4b0ec67994
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-03-01 18:49:19 +0300

    completing 'electronic watch' task

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -3,10 +3,18 @@ package com.epam.rd.autotasks.meetautocode;
 import java.util.Scanner;
 
 public class ElectronicWatch {
+
+    private final static int MINUTES_IN_A_HOUR = 60;
+    private final static int SECONDS_IN_A_MINUTE = 60;
+    private final static int HOURS_IN_A_DAY = 24;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int seconds = scanner.nextInt();
-        //
-
+        int time = scanner.nextInt();
+        int outputHours = time / (MINUTES_IN_A_HOUR * SECONDS_IN_A_MINUTE) % HOURS_IN_A_DAY;
+        int outputMinutes = time / SECONDS_IN_A_MINUTE % MINUTES_IN_A_HOUR;
+        int outputSeconds = time % SECONDS_IN_A_MINUTE;
+        String result = String.format("%d:%2d:%2d", outputHours, outputMinutes, outputSeconds).replace(' ', '0');
+        System.out.println(result);
     }
 }
