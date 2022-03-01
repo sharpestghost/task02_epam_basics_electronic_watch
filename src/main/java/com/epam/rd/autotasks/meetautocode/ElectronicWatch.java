1457b274dceafbac58db400995c811729b8683ba
Danil Dyachkov <danildyachkov13@gmail.com>
2022-03-01 06:00:16 +0000

    Update src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,7 +6,7 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-
+        //
 
     }
 }
